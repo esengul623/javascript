ecface6a9d4650fcaf261deaac350cc7ed1d2795
esengul <esenko2222@gmail.com>
2026-03-10 20:00:56 +0600

    java

---
+++ js.js
@@ -0,0 +1,13 @@
+let name = prompt("Введите ваше имя:");
+let surname = prompt("Введите вашу фамилию:");
+let age = prompt("Введите ваш возраст:");
+let gender = prompt("Введите ваш пол:");
+let height = prompt("Введите ваш рост:");
+let weight = prompt("Введите ваш вес:");
+
+console.log("Привет, " + name + "!");
+console.log("Ваша фамилия: " + surname);
+console.log("Вам " + age + " лет");
+console.log("Ваш пол: " + gender);
+console.log("Ваш рост: " + height + " см");
+console.log("Ваш вес: " + weight + " кг");
