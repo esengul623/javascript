1d90c2d836ab8338026e2966bbcca73fb2d5166b
esengul <esenko2222@gmail.com>
2026-03-16 22:56:50 +0600

    updata

--- js.js
+++ js.js
@@ -1,13 +1,42 @@
-let name = prompt("Введите ваше имя:");
-let surname = prompt("Введите вашу фамилию:");
-let age = prompt("Введите ваш возраст:");
-let gender = prompt("Введите ваш пол:");
-let height = prompt("Введите ваш рост:");
-let weight = prompt("Введите ваш вес:");
+// let name = prompt("Введите ваше имя:");
+// let surname = prompt("Введите вашу фамилию:");
+// let age = prompt("Введите ваш возраст:");
+// let gender = prompt("Введите ваш пол:");
+// let height = prompt("Введите ваш рост:");
+// let weight = prompt("Введите ваш вес:");
 
-console.log("Привет, " + name + "!");
-console.log("Ваша фамилия: " + surname);
-console.log("Вам " + age + " лет");
-console.log("Ваш пол: " + gender);
-console.log("Ваш рост: " + height + " см");
-console.log("Ваш вес: " + weight + " кг");
+// console.log("Привет, " + name + "!");
+// console.log("Ваша фамилия: " + surname);
+// console.log("Вам " + age + " лет");
+// console.log("Ваш пол: " + gender);
+// console.log("Ваш рост: " + height + " см");
+// console.log("Ваш вес: " + weight + " кг");
+// let arr = [1, [2, [3, { count: 4 }, [ 'не число', [5]]]]]
+// function getNumbers(data, result = []) {
+//   if (Array.isArray(data)) {
+//     for (let item of data) {
+//       getNumbers(item, result);
+//     }
+//   } else if (typeof data === "object" && data !== null) {
+//     for (let key in data) {
+//       getNumbers(data[key], result);
+//     }
+//   } else if (typeof data === "number") {
+//     result.push(data);
+//   }
+
+//   return result;
+// }
+
+// console.log(getNumbers(arr));
+
+
+// let age =  prompt ( 'выведите ваш возраст')
+// let peopel = prompt ( 'name')
+
+// if( age <=18 ){
+//     console.log( ' доступ разрешен')
+// }
+// else( peopel<=5 ){
+//     console.log( ' name ,у вас есть доступ')
+// }
